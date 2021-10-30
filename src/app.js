9e31abacdd6780ea7ebf4e75c30621ee11ddc80b
Afzal Shahul Hameed <afzal@freightwalla.com>
2021-10-30 12:53:00 +0530

    Write GET /contracts API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { Op } = require("sequelize");
 
 const { sequelize } = require('src/model')
 const { getProfile } = require('src/middleware/getProfile')
@@ -8,6 +9,28 @@ const app = express();
 app.use(express.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
+
+
+/**
+ * @returns all non-terminated contracts of a user
+ */
+app.get('/contracts', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models')
+
+    const contracts = await Contract.findAll({
+        where: {
+            [Op.or]: [
+                { ContractorId: req.profile.id },
+                { ClientId: req.profile.id }
+            ],
+            status: {
+                [Op.ne]: 'terminated'
+            }
+        }
+    })
+    res.json(contracts)
+})
+
 
 /**
  * @returns contract by id
--- test/api.contracts.test.js
+++ test/api.contracts.test.js
@@ -7,16 +7,113 @@ const app = require('src/app');
 
 chai.should()
 
+describe('GET /contracts', function () {
+    it('does not return terminated contracts', (done) => {
+        request(app)
+            .get('/contracts')
+            .set('Accept', 'application/json')
+            .set('profile_id', 5)
+            .expect(200)
+            .then(res => {
+                res.body.should.deep.equal([])
+                done()
+            })
+            .catch(done)
+    })
+
+    it('returns all non-terminated contracts of authorized contractor', (done) => {
+        request(app)
+            .get('/contracts')
+            .set('Accept', 'application/json')
+            .set('profile_id', 4)
+            .expect(200)
+            .then(res => {
+                for (c of res.body) {
+                    c.should.include.key('createdAt')
+                    c.should.include.key('updatedAt')
+                    delete c.createdAt
+                    delete c.updatedAt
+                }
+                res.body.should.deep.equal([
+                    {
+                        id: 7,
+                        terms: 'bla bla bla',
+                        status: 'in_progress',
+                        ClientId: 4,
+                        ContractorId: 7
+                    },
+                    {
+                        id: 8,
+                        terms: 'bla bla bla',
+                        status: 'in_progress',
+                        ClientId: 4,
+                        ContractorId: 6
+                    },
+                    {
+                        id: 9,
+                        terms: 'bla bla bla',
+                        status: 'in_progress',
+                        ClientId: 4,
+                        ContractorId: 8
+                    }
+                ])
+                done()
+            })
+            .catch(done)
+    })
+
+    it('returns all non-terminated contracts of authorized client', (done) => {
+        request(app)
+            .get('/contracts')
+            .set('Accept', 'application/json')
+            .set('profile_id', 6)
+            .expect(200)
+            .then(res => {
+                for (c of res.body) {
+                    c.should.include.key('createdAt')
+                    c.should.include.key('updatedAt')
+                    delete c.createdAt
+                    delete c.updatedAt
+                }
+                res.body.should.deep.equal([
+                    {
+                        id: 2,
+                        terms: 'bla bla bla',
+                        status: 'in_progress',
+                        ClientId: 1,
+                        ContractorId: 6
+                    },
+                    {
+                        id: 3,
+                        terms: 'bla bla bla',
+                        status: 'in_progress',
+                        ClientId: 2,
+                        ContractorId: 6
+                    },
+                    {
+                        id: 8,
+                        terms: 'bla bla bla',
+                        status: 'in_progress',
+                        ClientId: 4,
+                        ContractorId: 6
+                    }
+                ])
+                done()
+            })
+            .catch(done)
+    })
+})
+
 describe('GET /contracts/{id}', function () {
-    it('returns with 404 if id is non existent', (done) => {
+    it('raises 404 if id is non existent', (done) => {
         request(app)
             .get('/contracts/10')
             .set('Accept', 'application/json')
-            .set('profile_id', 4)
+            .set('profile_id', 5)
             .expect(404, done)
     });
 
-    it('returns with 404 if contract exists but does not belong to authorized profile', (done) => {
+    it('raises 404 if contract exists but does not belong to authorized profile', (done) => {
         request(app)
             .get('/contracts/1')
             .set('Accept', 'application/json')
@@ -24,7 +121,7 @@ describe('GET /contracts/{id}', function () {
             .expect(404, done)
     });
 
-    it('returns with 200 if contract exists and belongs to authorized profile', (done) => {
+    it('returns contract if it exists and belongs to authorized profile', (done) => {
         request(app)
             .get('/contracts/1')
             .set('Accept', 'application/json')
@@ -44,5 +141,6 @@ describe('GET /contracts/{id}', function () {
                 })
                 done()
             })
+            .catch(done)
     });
 });
